b869f6cf8bf9f2002de0a5fcd7f3b58f01168ea5
Sebastiam Delgado <sebaseldemente9443@outlook.com>
2019-01-03 09:42:38 -0500

    examen terminado y corregido.

--- DelgadoEddyParcial2/DelgadoEddyParcial2/Program.cs
+++ DelgadoEddyParcial2/DelgadoEddyParcial2/Program.cs
@@ -8,39 +8,90 @@ namespace DelgadoEddyParcial2
 {
     class Program
     {
+        const int max = 100;
         static void Main(string[] args)
         {
-            PRegunta1();
+            int[] vector = new int[max];
+            llenarvector(vector);
+            ordenarVector(vector);
+            imprimirVector(vector);
+            Console.WriteLine("Valor maximo: {0}", vector[0]);
+            Console.WriteLine("Valor minimo: {0}", vector[max-1]);
+            Console.WriteLine("Sumatoria {0}",sumatoria(vector));
+            Console.WriteLine("Promedio {0}", promedio(vector));
+            Console.WriteLine("Total de decenas: {0}", ContarDecenas(vector));
+            Console.WriteLine("Desv. Estamdar: {0}", DesvEstandar(vector));
             Console.ReadKey();
         }
-        public static void PRegunta1()
+        public static double DesvEstandar(int []x)
+        {
+            double res = 0;
+            double Pro = promedio(x);
+            for(int i=0;i<max;i++)
+            {
+                res=res+(x[i] - Pro) * (x[i] - Pro);
+            }
+            return Math.Sqrt(res / (max * 1));
+        }
+        public static int ContarDecenas(int []x)
+        {
+            int c = 0;
+            for (int i=0;i<max;i++)
+            {
+                if(x[i]>=10 && x[i]<=90)
+                {
+                    if (x[i] % 10 == 0)
+                        c++;
+                }
+            }
+            return c;
+        }
+        public static double promedio(int []x)
+        {
+            double suma = sumatoria(x);
+            return (suma / max);
+        }
+        public static int sumatoria(int []x)
+        {
+            int suma = 0;
+            for (int i = 0; i < max; i++)
+                suma = suma + x[i];
+            return suma;
+        }
+        public static void llenarvector(int[]x)
 
         {
-            int[] V = new int[100];
-            int mayor = V[0], menor = V[0];
-            for (int i = 0; i < 100; i--)
+            Random r = new Random();
+            for (int i=0;i<max;i++)
             {
-                Console.WriteLine("\n ",V[i]);
-            }
-            for (int i = 0; i < 100; i++)
-            {
-                if (V[i] > mayor)
-                {
-                    mayor = V[i];
-                }
-            }
-            menor = mayor;
-            for (int i = 0; i < 100; i++)
-            {
-                if (V[i] < menor)
-                {
-                    menor = V[i];
-                }
+                int numero = r.Next(1, 100);
+                x[i]= numero;
             }
             
-            Console.WriteLine("El elemento mayor es: " + mayor);
-            Console.WriteLine("El elemento menor es: " + menor);
+        }
+        public static void imprimirVector(int[]x)
+        {
+            Console.WriteLine("Elementos del vector: ");
+            for(int i=0;i<max;i++)
+            {
+                Console.WriteLine("{0} ", x[i]);
+            }
+            Console.WriteLine();
+        }
+        public static void ordenarVector(int[]x)
+        {
+            int aux = 0;
+            for(int i=0;i<max;i++)
+            for(int j=i+1;j<max;j++)
+            {
+                if (x[i]<x[j])
+                {
+                    aux = x[i];
+                    x[i] = x[j];
+                    x[j] = aux;
 
+                }
+            }
         }
     }
 }
